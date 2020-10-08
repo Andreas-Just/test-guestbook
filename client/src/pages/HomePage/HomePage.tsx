12d1d04880f912d6d95f8d3c539ea49d502a641e
Andreas_Just <andreas.wq@gmail.com>
2020-10-08 04:51:51 +0300

    HomePages was fixed

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -4,13 +4,13 @@ import './HomePage.scss';
 export const HomePage = () => {
   return (
     <div className="HomePage row">
-      <h2>Greetings dear visitor</h2>
+      <h2 className="HomePage-Title">Greetings dear visitor</h2>
       <div className="col s8 offset-s2">
-        <p>Welcome to the app!</p>
-        <p>This is an example of a guestbook that used to be popular on presentation sites.</p>
-        <p>You can leave any feedback on the create page and see all reviews on the list page.</p>
-        <p>When creating the application, the following technology stack was used:</p>
-        <p>MongoDB, Express, React, Node.js,
+        <p className="HomePage-Text">Welcome to the app!</p>
+        <p className="HomePage-Text">This is an example of a guestbook that used to be popular on presentation sites.</p>
+        <p className="HomePage-Text">You can leave any feedback on the create page and see all reviews on the list page.</p>
+        <p className="HomePage-Text">When creating the application, the following technology stack was used:</p>
+        <p className="HomePage-Text">MongoDB, Express, React, Node.js,
           TypeScript.</p>
       </div>
     </div>
